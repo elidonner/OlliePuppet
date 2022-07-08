10daaecc1713ebe54fab782c72e8974ead857a57
ethandonlon <94150522+ethandonlon@users.noreply.github.com>
2022-07-08 11:30:18 -0400

    added different compoenents that were on school computer

---
+++ tests/servo_calibration/servo_calibration.ino
@@ -0,0 +1,51 @@
+// We'll track the volume level in this variable.
+float servo_pulse = 1.0;
+
+
+/**** SERVOSTUFF *****/
+#define SERVOPIN 25
+// 40KHz > freq*resolution > 40MHz
+#define LEDC_RES_BITS 12        // cannot be higher than 14 bits
+#define LEDC_RES ((1<<LEDC_RES_BITS)-1)
+#define LEDC_FREQ_HZ 60
+#define CHANNEL0 0
+
+void servoSetup(int pin) {
+  ledcSetup(CHANNEL0, LEDC_FREQ_HZ, LEDC_RES_BITS); // channel, freq, bits
+  ledcAttachPin(pin, CHANNEL0);
+}
+
+void sendServo(float pulse_ms) {  // 1.0ms = 90o left, 2.0ms = 90o right, 1.5ms center
+  ledcWrite(CHANNEL0, (pulse_ms / 1000.0)*LEDC_FREQ_HZ * LEDC_RES);
+}
+
+void setup() {
+  Serial.begin(115200);
+  servoSetup(SERVOPIN);
+}
+
+// Read in + and - characters to set the volume.
+void loop() {
+  if (! Serial.available()) return;
+  
+  // read a character from serial console
+  char c = Serial.read();
+  
+  // increase
+  if (c == '+') {
+    servo_pulse+=0.01;
+  }
+  // decrease
+  else if (c == '-') {
+    servo_pulse-=0.01;
+  }
+  // ignore anything else
+  else 
+    return;
+    
+  servo_pulse = constrain(servo_pulse,.8,2.0);
+
+  Serial.println(servo_pulse);
+
+  sendServo(servo_pulse);
+}
